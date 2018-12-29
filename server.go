05e79a5db54733769d70251963e269274d5d9b4f
mentix02 <manan.yadav02@gmail.com>
2018-12-29 21:28:07 +0530

    removed redundant error code and documented it

--- server.go
+++ server.go
@@ -7,38 +7,54 @@ import (
 	"strings"
 )
 
+// opens a new connection
+// every time a client is
+// connected to tcp port
+// of 127.0.0.1:4998
 func serve(c net.Conn) {
+	// log a message of connection
 	log.Println("established connection with", c.RemoteAddr().String())
 
 	for {
+		// get data from
+		// client connection
 		remoteData, err := bufio.NewReader(c).ReadString('\n')
+		check(err)
 
-		if err != nil {
-			break
-		}
-
+		// remove all unnecessary spaces
 		command := strings.TrimSpace(string(remoteData))
 
+		// if only the return
+		// key was pressed as
+		// input then no response
+		// will be received by
+		// the client connected
 		if len(command) == 0 {
 			continue
 		}
 
+		// close tcp connection
+		// to the client
 		if command == "bye" || command == "exit" {
 			_, _ = c.Write([]byte("bye\n"))
 			break
 		}
 
+		// get response as an
+		// error response
+		// or a success response
 		response, err := executor(command, &c)
 
+		// if error was received
+		// as a response then
+		// an error type response
+		// is issued to the client
 		if err != nil {
-			_, _ = c.Write([]byte(err.Error()))
+			_, _ = c.Write([]byte("error :" + err.Error()))
 		}
 
-		if len(response) == 0 {
-			_, _ = c.Write([]byte("\n"))
-			continue
-		}
-
+		// send back response
+		// with successful execution
 		_, _ = c.Write([]byte(response + "\n"))
 
 	}
