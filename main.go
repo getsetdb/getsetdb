3a7d01ef7fa520555cf6a7b7c5f9a7288c3c670f
mentix02 <manan.yadav02@gmail.com>
2018-12-29 21:25:40 +0530

    added a const port number

--- main.go
+++ main.go
@@ -5,9 +5,11 @@ import (
 	"net"
 )
 
+const port string = ":4998"
+
 func main() {
 
-	ln, err := net.Listen("tcp4", ":4444")
+	ln, err := net.Listen("tcp4", port)
 	check(err)
 
 	log.Println("tcp server running on 127.0.0.1:4444")
