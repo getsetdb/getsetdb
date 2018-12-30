41e717d28dd9a460e3dbe4111bbb33d0b818b6de
mentix02 <manan.yadav02@gmail.com>
2018-12-30 13:29:46 +0530

    formatted pretty response

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	ln, err := net.Listen("tcp4", port)
 	check(err)
 
-	log.Println("tcp server running on 127.0.0.1:4444")
+	log.Printf("tcp server running on 127.0.0.1%s\n", port)
 
 	defer ln.Close()
 
